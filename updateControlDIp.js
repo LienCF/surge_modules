7b7273546be9cef1e70186b3d78efb8fae3f00ff
AlexLien <alien@d2nova.com>
2021-07-28 22:03:22 +0800

    add notification

--- updateControlDIp.js
+++ updateControlDIp.js
@@ -1,3 +1,4 @@
 $httpClient.get('https://dns.controld.com/er3xe8ecp8?dns=q80BAAABAAAAAAAAA3d3dwdleGFtcGxlA2NvbQAAAQAB', function(error, response, data){
+	$notification.post('ControlD IP Update', data.split(';'));
     $done({});
 });
