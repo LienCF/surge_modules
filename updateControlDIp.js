b61a5f982c86258ce809aed67723bf672637bef6
LienCF <5109608+LienCF@users.noreply.github.com>
2021-10-06 13:25:25 +0800

    Update updateControlDIp.js

--- updateControlDIp.js
+++ updateControlDIp.js
@@ -1,4 +1,4 @@
 $httpClient.get('https://ultralow.dns2.nextdns.io/56b924/DNS_Update?dns=q80BAAABAAAAAAAAA3d3dwdleGFtcGxlA2NvbQAAAQAB', function(error, response, data){
-    $notification.post('ControlD IP Update', data.split(';'));
+    $notification.post('ControlD IP Update');
     $done({});
 });
