dcb7d5d5cded1a60a8cdc87724057b219d53e5eb
LienCF <liencf@gmail.com>
2024-08-08 19:45:57 +0800

    update to use token

--- E2NovaCheckIn.js
+++ E2NovaCheckIn.js
@@ -1,5 +1,4 @@
 // E2NovaCheckIn.js
-const URL_LOGIN = "https://www.femascloud.com/e2nova/Accounts/login";
 const URL_CHECKIN = "https://www.femascloud.com/e2nova/users/clock_listing";
 const URL_CHECK_EVENT = "https://femashr-app-api.femascloud.com/e2nova/fsapi/V3/calendar.json";
 const METHOD = "POST";
@@ -14,27 +13,19 @@ function getCurrentTimePlus30Mins() {
 
 function getHeaders(randomString, currentTimePlus30Mins) {
     return {
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "Accept-Language": "en-US,en;q=0.9,zh-TW;q=0.8,zh;q=0.7",
-        "Cache-Control": "no-cache",
-        "Connection": "keep-alive",
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Cookie": `e2nova=${randomString}; CakeCookie[e2novalanguage]=Q2FrZQ%3D%3D.FFwdiDU%3D; CakeCookie[5][e2novalanguage]=Q2FrZQ%3D%3D.FFwdiDU%3D; setTopMenu=menu_3; menuBlock=16; menuItem=2; lifeTimePointe2nova=${currentTimePlus30Mins}; femasUid=E20003; femasRem=1`,
-        "Origin": "https://www.femascloud.com",
-        "Pragma": "no-cache",
-        "Referer": "https://www.femascloud.com/e2nova/accounts/login",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "same-origin",
-        "Sec-Fetch-User": "?1",
-        "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
-        "sec-ch-ua": "\"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\"",
-        "sec-ch-ua-mobile": "?0",
+        ":authority": "femashr-app-api.femascloud.com",
+        "content-type": "application/json",
+        "accept": "*/*",
+        "authorization": $persistentStore.read('e2nova_token'),
+        "x-client-version": "3.4.3",
+        "x-client-device": "IOS",
+        "content-language": "zh-tw",
+        "accept-language": "zh-Hant-TW;q=1.0, en-TW;q=0.9",
+        "user-agent": "FemasHR/3.4.3 (com.femascloud.femashr; build:5; iOS 17.6.0) Alamofire/5.8.0",
+        "cookie": `e2nova=${randomString}`
     };
 }
 
-const LOGIN_BODY = "data%5BAccount%5D%5Busername%5D=E20003&data%5BAccount%5D%5Bpasswd%5D=mQ8DYYgybsFHaE&data%5Bremember%5D=0&data%5Bremember%5D=1";
 const CHECKIN_BODY = "_method=POST&data%5BClockRecord%5D%5Buser_id%5D=4&data%5BAttRecord%5D%5Buser_id%5D=4&data%5BClockRecord%5D%5Bshift_id%5D=9&data%5BClockRecord%5D%5Bperiod%5D=1&data%5BClockRecord%5D%5Bclock_type%5D=S&data%5BClockRecord%5D%5Blatitude%5D=&data%5BClockRecord%5D%5Blongitude%5D=";
 const CHECKOUT_BODY = "_method=POST&data%5BClockRecord%5D%5Buser_id%5D=4&data%5BAttRecord%5D%5Buser_id%5D=4&data%5BClockRecord%5D%5Bshift_id%5D=9&data%5BClockRecord%5D%5Bperiod%5D=1&data%5BClockRecord%5D%5Bclock_type%5D=E&data%5BClockRecord%5D%5Blatitude%5D=&data%5BClockRecord%5D%5Blongitude%5D=";
 const CHECK_EVENT_BODY = '{"eventIsNotOver":true,"type":"user"}';
@@ -116,7 +107,6 @@ function checkEvent(headers, callback) {
 }
 
 function main() {
-    console.log('Test: 1');
     const randomString = generateRandomString();
     const currentTimePlus30Mins = getCurrentTimePlus30Mins();
     console.log(`Random String: ${randomString}, Time Plus 30 Mins: ${currentTimePlus30Mins}`);
@@ -124,28 +114,21 @@ function main() {
     const headers = getHeaders(randomString, currentTimePlus30Mins);
     console.log(`Headers: ${JSON.stringify(headers)}`);
 
-    $httpClient.post({url: URL_LOGIN, headers: headers, body: LOGIN_BODY, timeout: 50}, function (error, response, data) {
-        if (error) {
-            handleResponse(error, response, data);
-        } else {
-            console.log(`Response: ${JSON.stringify(response)}`);
-            const taipeiHour = getCurrentTaipeiHour();
-            console.log(`Current Taipei hour: ${taipeiHour}`);
-            
-            const bodyToUse = getBodyBasedOnTime(taipeiHour);
+    const taipeiHour = getCurrentTaipeiHour();
+    console.log(`Current Taipei hour: ${taipeiHour}`);
+    
+    const bodyToUse = getBodyBasedOnTime(taipeiHour);
 
-            checkEvent(headers, function(skip) {
-                if (skip) {
-                    $done({});
-                } else {
-                    if (bodyToUse !== null) {
-                        $httpClient.post({url: URL_CHECKIN, headers: headers, body: bodyToUse, timeout: 50}, handleResponse);
-                    } else {
-                        console.log('Not in the check-in/out time, skipping check-in/out');
-                        $done({});
-                    }
-                }
-            });
+    checkEvent(headers, function(skip) {
+        if (skip) {
+            $done({});
+        } else {
+            if (bodyToUse !== null) {
+                $httpClient.post({url: URL_CHECKIN, headers: headers, body: bodyToUse, timeout: 50}, handleResponse);
+            } else {
+                console.log('Not in the check-in/out time, skipping check-in/out');
+                $done({});
+            }
         }
     });
 }
