22ce9ac263ad57a8b0e87e49634caea176003ce5
LienCF <liencf@gmail.com>
2024-08-08 19:30:38 +0800

    store the token for use

---
+++ E2NovaGetToken.js
@@ -0,0 +1,21 @@
+const $ = new Env('E2Nova Token');
+
+function getToken() {
+    if ($request.method === 'POST') {
+        const headers = $request.headers;
+        const authorization = headers['Authorization'] || headers['authorization'];
+        
+        if (authorization) {
+            $persistentStore.write(authorization, 'e2nova_token');
+            $notification.post('E2Nova Token', 'Token successfully saved', authorization);
+        } else {
+            $notification.post('E2Nova Token', 'Failed to save token', 'Authorization header not found');
+        }
+    } else {
+        $notification.post('E2Nova Token', 'Failed to save token', 'Request method is not POST');
+    }
+}
+
+getToken();
+
+$done({});
